2137cae02fe9629fd231754509c2840803a8296f
Chris Baldelomar <chris@webplantmedia.com>
2014-12-08 12:42:50 -0600

    1.0.5

--- content-one-line.php
+++ content-one-line.php
@@ -5,7 +5,13 @@
 ?>
 <?php 
 $show_post_meta = false;
+$is_basic = false;
 $class = 'hide-post-meta';
+
+if ( has_tag('basic') ) {
+	$class .= ' basic-tutorial';
+	$is_basic = true;
+}
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class( $class . ' wpc2-post' ); ?>>
@@ -14,4 +20,15 @@ $class = 'hide-post-meta';
 		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
 
 	</header><!-- .entry-header -->
+
+	<?php if ( $is_basic ) : ?>
+		<div class="entry-summary clear">
+
+			<div class="entry-excerpt">
+				<?php the_excerpt(); ?>
+			</div>
+
+		</div><!-- .entry-summary -->
+	<?php endif; ?>
+
 </article><!-- #post-## -->
