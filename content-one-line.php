13c18d922a03976ecf3e9ab46aabfca8a9b3fbdd
Chris Baldelomar <chris@webplantmedia.com>
2014-12-08 23:57:36 -0600

    1.0.8

--- content-one-line.php
+++ content-one-line.php
@@ -5,13 +5,7 @@
 ?>
 <?php 
 $show_post_meta = false;
-$is_basic = false;
 $class = 'hide-post-meta';
-
-if ( has_tag('basic') ) {
-	$class .= ' basic-tutorial';
-	$is_basic = true;
-}
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class( $class . ' wpc2-post' ); ?>>
@@ -20,15 +14,4 @@ if ( has_tag('basic') ) {
 		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
 
 	</header><!-- .entry-header -->
-
-	<?php if ( $is_basic ) : ?>
-		<div class="entry-summary clear">
-
-			<div class="entry-excerpt">
-				<?php the_excerpt(); ?>
-			</div>
-
-		</div><!-- .entry-summary -->
-	<?php endif; ?>
-
 </article><!-- #post-## -->
