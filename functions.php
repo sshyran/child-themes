7b67bdf8ceba0e299ee0431b313cfdb3a1b290da
Chris Baldelomar <chris@webplantmedia.com>
2015-06-08 10:57:00 -0500

    search by title only

--- functions.php
+++ functions.php
@@ -27,6 +27,35 @@ function wpcanvas2_child_setup() {
 }
 add_action( 'after_setup_theme', 'wpcanvas2_child_setup' );
 
+function wpcanvas2_child_search_by_title_only( $search, &$wp_query ) {
+    global $wpdb;
+
+    if ( empty( $search ) ) {
+        return $search; // skip processing - no search term in query
+    }
+
+    $q = $wp_query->query_vars;
+    $n = ! empty( $q['exact'] ) ? '' : '%';
+    $search =
+    $searchand = '';
+
+    foreach ( (array)$q['search_terms'] as $term ) {
+        $term = esc_sql( $wpdb->esc_like( $term ) );
+        $search .= "{$searchand}($wpdb->posts.post_title LIKE '{$n}{$term}{$n}')";
+        $searchand = ' AND ';
+    }
+
+    if ( !empty( $search ) ) {
+        $search = " AND ({$search}) ";
+        if ( ! is_user_logged_in() ) {
+            $search .= " AND ($wpdb->posts.post_password = '') ";
+		}
+    }
+
+    return $search;
+}
+add_filter( 'posts_search', 'wpcanvas2_child_search_by_title_only', 500, 2 );
+
 /**
  * Create HTML list of categories.
  *
